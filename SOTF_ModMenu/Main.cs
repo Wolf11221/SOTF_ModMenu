2ce4879f6bf9b7e3311e5d2b6e0f5c9dccd4d034
Woodensquid <jacobv93@msn.com>
2023-03-12 21:57:45 -0400

    add repair structure ESP, minor adjustments

--- SOTF_ModMenu/Main.cs
+++ SOTF_ModMenu/Main.cs
@@ -1,3 +1,4 @@
+using Construction;
 using Il2CppSystem.Collections.Generic;
 using Il2CppSystem.IO;
 using Sons.Input;
@@ -18,6 +19,9 @@ namespace SOTF_ModMenu
             static bool isInitialized = false;
             private Vector2 scrollPosition = Vector2.zero;
             public static Camera _cameraMain;
+            public static HashSet<Structure> _dirtyStructures;
+            public static Scene _sonsMainScene;
+            private Vitals vitals;
 
             private void OnGUI()
             {
@@ -26,17 +30,13 @@ namespace SOTF_ModMenu
                 if (Settings.EspEnable)
                     ESP.Enabled();
                 else
-                {
-                    Settings.EspAnimalsEnable = false;
-                    Settings.EspEnemyEnable = false;
-                    Settings.EspFriendlyEnable = false;
-                }
-                
+                    ESP.Disabled();
+
                 if(!Settings.Visible) return;
                 GUI.color = Color.white;
                 
                 //Player
-                UIHelper.Begin("Player", 10, 10, 165, 237, 2, 20, 2);
+                UIHelper.Begin("Player", 10, 10, 165, 241, 2, 20, 2);
                 Settings.Health = UIHelper.Button("Max Health: ", Settings.Health);
                 Settings.Stamina = UIHelper.Button("Max Stamina: ", Settings.Stamina);
                 Settings.Strength = UIHelper.Button("Max Strength: ", Settings.Strength);
@@ -55,11 +55,12 @@ namespace SOTF_ModMenu
                 Settings.CaveLight = UIHelper.Button("Cave Light: ", Settings.CaveLight);  
                 
                 //ESP
-                UIHelper.Begin("ESP", 350, 10, 165, 108, 2, 20, 2);
-                Settings.EspEnable = UIHelper.Button("ESP Enabled: ", Settings.EspEnable);
-                Settings.EspAnimalsEnable = UIHelper.Button("ESP Animals: ", Settings.EspAnimalsEnable);
-                Settings.EspEnemyEnable = UIHelper.Button("ESP Enemies: ", Settings.EspEnemyEnable);
-                Settings.EspFriendlyEnable = UIHelper.Button("ESP Friendly: ", Settings.EspFriendlyEnable);
+                UIHelper.Begin("ESP", 350, 10, 165, 130, 2, 20, 2);
+                Settings.EspEnable = UIHelper.Button("Enabled: ", Settings.EspEnable);
+                Settings.EspAnimalsEnable = UIHelper.Button("Animals: ", Settings.EspAnimalsEnable);
+                Settings.EspEnemyEnable = UIHelper.Button("Enemies: ", Settings.EspEnemyEnable);
+                Settings.EspFriendlyEnable = UIHelper.Button("Friendly: ", Settings.EspFriendlyEnable);
+                Settings.EspStructureDamage = UIHelper.Button("Structure Damage: ", Settings.EspStructureDamage);
                 
                 //Other
                 UIHelper.Begin("Other", 520, 10, 165, 100, 2, 20, 2);
@@ -67,7 +68,7 @@ namespace SOTF_ModMenu
                 //Settings.FreeCam = UIHelper.Button("FreeCam Enabled: ", Settings.FreeCam);
 
                 //Item Spawner
-                UIHelper.Begin("Item Spawner", 10, 252, 165, 84, 2, 20, 2);
+                UIHelper.Begin("Item Spawner", 10, 256, 165, 84, 2, 20, 2);
                 UIHelper.Label("Enter id & amount");
                 Settings.TextFieldItemID = GUI.TextField(new Rect(12, 292, 40, 20), Settings.TextFieldItemID);
                 Settings.TextFieldAmount = GUI.TextField(new Rect(55, 292, 30, 20), Settings.TextFieldAmount);
@@ -76,7 +77,7 @@ namespace SOTF_ModMenu
                 if (GUI.Button(new Rect(12, 314, 161, 20), "Show All ID's"))
                     Settings.ShowItemIDs = !Settings.ShowItemIDs;
                 
-                GUI.backgroundColor=Color.grey;
+                GUI.backgroundColor = Color.grey;
                 //Show IDs Window
                 if (Settings.ShowItemIDs)
                     windowRect = GUI.Window(0, windowRect, (GUI.WindowFunction)ShowAllIDsWindow, "Show ID's");
@@ -86,8 +87,10 @@ namespace SOTF_ModMenu
             {
                 RegisterHandlers();
                 
-                //continue getting player camera object to use for position in world
-                _cameraMain = Camera.main ?? null;
+                //cache SonsMainScene
+                if(!_sonsMainScene.isLoaded) _sonsMainScene = SceneManager.GetSceneByName("SonsMain");
+                //Cache CameraMain
+                if(_cameraMain == null) _cameraMain = Camera.main ?? null;
                 
                 //player in world
                 if (!LocalPlayer.IsInWorld) return;
@@ -105,8 +108,7 @@ namespace SOTF_ModMenu
                 CPlayer.CaveLight(Settings.CaveLight);
 
                 //Vitals
-                if(vitals == null)
-                    vitals = LocalPlayer.Vitals;
+                if(vitals == null) vitals = LocalPlayer.Vitals;
                 
                 if (Settings.Health) {
                     vitals._health._currentValue = vitals._health._max;
@@ -196,7 +198,7 @@ namespace SOTF_ModMenu
             private void HideHUD()
             {
                 Settings.HideHUD = !Settings.HideHUD;
-                foreach (GameObject gObject in SceneManager.GetSceneByName("SonsMain").GetRootGameObjects())
+                foreach (GameObject gObject in _sonsMainScene.GetRootGameObjects())
                 {
                     if (gObject.name == "PlayerStandin")
                     {
@@ -211,8 +213,7 @@ namespace SOTF_ModMenu
             private Rect windowRect = new Rect(10, 345, 300, 500);
             public void ShowAllIDsWindow (int windowID)
             {
-                sonsMainScene = SceneManager.GetSceneByName("SonsMain");
-                if (sonsMainScene.isLoaded)
+                if (_sonsMainScene.isLoaded)
                 {
                     if (!isInitialized)
                     {
@@ -246,9 +247,6 @@ namespace SOTF_ModMenu
                 
                 GUI.DragWindow(new Rect(0, 0, 10000, 20));
             }
-
-            private Scene sonsMainScene;
-            private Vitals vitals;
         }
     }
 }
--- SOTF_ModMenu/Utilities/Settings.cs
+++ SOTF_ModMenu/Utilities/Settings.cs
@@ -1,42 +1,42 @@
-﻿namespace SOTF_ModMenu.Utilities
+﻿namespace SOTF_ModMenu.Utilities;
+
+public static class Settings
 {
-    public static class Settings
-    {
-        public static bool Visible = false;
-        public static bool ShowItemIDs = false;
-        public static bool HideHUD = false;
-        
-        public static bool Stamina = false;
-        public static bool Thirst = false;
-        public static bool Health = false;
-        public static bool Hunger = false;
-        public static bool Rested = false;
-        public static bool Cold = false;
-        public static bool Strength = false;
-        public static bool LungCapacity = false;
-        
-        public static bool InstantBuild = false;
-        public static bool InfBuild = false;
-        
-        //ESP Settings
-        public static bool EspEnable = true;
-        public static bool EspAnimalsEnable = false;
-        public static bool EspEnemyEnable = false;
-        public static bool EspFriendlyEnable = false;
-        
-        //InfLog Settings
-        public static bool InfLogs = false;
-        
-        //InfAmmo
-        public static bool InfAmmo = false;
-        
-        //SpeedyRun
-        public static bool SpeedyRun = false;
-        
-        //CaveLight
-        public static bool CaveLight = false;
-        
-        public static string TextFieldItemID = "392";
-        public static string TextFieldAmount = "1";
-    }
+    public static bool Visible = false;
+    public static bool ShowItemIDs = false;
+    public static bool HideHUD = false;
+
+    public static bool Stamina = false;
+    public static bool Thirst = false;
+    public static bool Health = false;
+    public static bool Hunger = false;
+    public static bool Rested = false;
+    public static bool Cold = false;
+    public static bool Strength = false;
+    public static bool LungCapacity = false;
+
+    public static bool InstantBuild = false;
+    public static bool InfBuild = false;
+
+    //ESP Settings
+    public static bool EspEnable = true;
+    public static bool EspAnimalsEnable = false;
+    public static bool EspEnemyEnable = false;
+    public static bool EspFriendlyEnable = false;
+    public static bool EspStructureDamage = false;
+
+    //InfLog Settings
+    public static bool InfLogs = false;
+
+    //InfAmmo
+    public static bool InfAmmo = false;
+
+    //SpeedyRun
+    public static bool SpeedyRun = false;
+
+    //CaveLight
+    public static bool CaveLight = false;
+
+    public static string TextFieldItemID = "392";
+    public static string TextFieldAmount = "1";
 }
