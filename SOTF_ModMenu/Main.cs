e0dfd1af508abd6722e00c14d1d8f8e023b60dd7
Nie <76068682+Wolf11221@users.noreply.github.com>
2023-04-23 23:33:35 +0200

    Libs Update

--- SOTF_ModMenu/Main.cs
+++ SOTF_ModMenu/Main.cs
@@ -17,9 +17,6 @@ namespace SOTF_ModMenu
     {
         public class MyMonoBehaviour : MonoBehaviour
         {
-            static List<ItemData> itemList;
-            static bool isInitialized = false;
-            private Vector2 scrollPosition = Vector2.zero;
             public static Camera _cameraMain;
             public static HashSet<Structure> _dirtyStructures;
             public static Scene _sonsMainScene;
@@ -27,62 +24,7 @@ namespace SOTF_ModMenu
 
             private void OnGUI()
             {
-                
-                //ESP Draw, placed before check for cheat visible to prevent the ESP from not rendering when GUI not visible
-                if (Settings.EspEnable)
-                    ESP.Enabled();
-                else 
-                    ESP.Disabled();
-
-                if(!Settings.Visible) return;
-                GUI.color = Color.white;
-                
-                //Player
-                UIHelper.Begin("Player", 10, 10, 165, 241, 2, 20, 2);
-                Settings.Health = UIHelper.Button("Max Health: ", Settings.Health);
-                Settings.Stamina = UIHelper.Button("Max Stamina: ", Settings.Stamina);
-                Settings.Strength = UIHelper.Button("Max Strength: ", Settings.Strength);
-                Settings.LungCapacity = UIHelper.Button("Max LungCapacity: ", Settings.LungCapacity);
-                Settings.Cold = UIHelper.Button("No Cold: ", Settings.Cold);
-                Settings.Hunger = UIHelper.Button("No Hunger: ", Settings.Hunger);
-                Settings.Thirst = UIHelper.Button("No Thirst: ", Settings.Thirst);
-                Settings.Rested = UIHelper.Button("Always Rested: ", Settings.Rested);
-                Settings.InfAmmo = UIHelper.Button("Infinite Ammo: ", Settings.InfAmmo);
-                Settings.SpeedyRun = UIHelper.Button("SpeedRun: ", Settings.SpeedyRun);
-
-                //World
-                UIHelper.Begin("World", 180, 10, 165, 100, 2, 20, 2);
-                Settings.InstantBuild = UIHelper.Button("Instant Build: ", Settings.InstantBuild);  
-                Settings.InfBuild = UIHelper.Button("Infinite Build: ", Settings.InfBuild);  
-                Settings.CaveLight = UIHelper.Button("Cave Light: ", Settings.CaveLight);  
-                
-                //ESP
-                UIHelper.Begin("ESP", 350, 10, 165, 130, 2, 20, 2);
-                Settings.EspEnable = UIHelper.Button("Enabled: ", Settings.EspEnable);
-                Settings.EspAnimalsEnable = UIHelper.Button("Animals: ", Settings.EspAnimalsEnable);
-                Settings.EspEnemyEnable = UIHelper.Button("Enemies: ", Settings.EspEnemyEnable);
-                Settings.EspFriendlyEnable = UIHelper.Button("Friendly: ", Settings.EspFriendlyEnable);
-                Settings.EspStructureDamage = UIHelper.Button("Structure Damage: ", Settings.EspStructureDamage);
-                
-                //Other
-                UIHelper.Begin("Other", 520, 10, 165, 100, 2, 20, 2);
-                Settings.InfLogs = UIHelper.Button("Infinite Logs: ", Settings.InfLogs);
-                //Settings.FreeCam = UIHelper.Button("FreeCam Enabled: ", Settings.FreeCam);
-
-                //Item Spawner
-                UIHelper.Begin("Item Spawner", 10, 256, 165, 84, 2, 20, 2);
-                UIHelper.Label("Enter id & amount");
-                Settings.TextFieldItemID = GUI.TextField(new Rect(12, 292, 40, 20), Settings.TextFieldItemID);
-                Settings.TextFieldAmount = GUI.TextField(new Rect(55, 292, 30, 20), Settings.TextFieldAmount);
-                if (GUI.Button(new Rect(88, 292, 85, 20), "Spawn"))
-                    SpawnItem();
-                if (GUI.Button(new Rect(12, 314, 161, 20), "Show All ID's"))
-                    Settings.ShowItemIDs = !Settings.ShowItemIDs;
-                
-                GUI.backgroundColor = Color.grey;
-                //Show IDs Window
-                if (Settings.ShowItemIDs)
-                    windowRect = GUI.Window(0, windowRect, (GUI.WindowFunction)ShowAllIDsWindow, "Show ID's");
+                UI.UIManager.Display();
             }
 
             private void Update()
@@ -184,7 +126,7 @@ namespace SOTF_ModMenu
                 }
             }
             
-            private void SpawnItem()
+            public static void SpawnItem()
             {
                 try
                 {
@@ -209,46 +151,6 @@ namespace SOTF_ModMenu
                         break;
                     }
                 }
-            }
-
-            // Add search, categories
-            // Optimize this
-            private Rect windowRect = new Rect(10, 345, 300, 500);
-            public void ShowAllIDsWindow (int windowID)
-            {
-                if (_sonsMainScene.isLoaded)
-                {
-                    if (!isInitialized)
-                    {
-                        itemList ??= ItemDatabaseManager.Items;
-                        isInitialized = true;
-                    }
-                }
-
-                if (itemList == null || itemList.Count == 0)
-                {
-                    GUI.Label(new Rect(5, 15, 300, 500), "Item list is empty.");
-                }
-                else
-                {
-                    var writer = new StringWriter();
-                    foreach (ItemData item in itemList)
-                    {
-                        writer.Write(item._name);
-                        writer.Write(" : ");
-                        writer.WriteLine(item._id);
-                    }
-
-                    GUILayout.BeginArea(new Rect(5, 15, 295, 480));
-                    scrollPosition = GUILayout.BeginScrollView(scrollPosition);
-                    GUILayout.BeginVertical();
-                    GUILayout.Label(writer.ToString());
-                    GUILayout.EndVertical();
-                    GUILayout.EndScrollView();
-                    GUILayout.EndArea();
-                }
-                
-                GUI.DragWindow(new Rect(0, 0, 10000, 20));
             }
         }
     }
